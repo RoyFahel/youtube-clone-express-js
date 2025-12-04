764565b2d308350e85803862a3c82da8bd6479df
RoyFahel <167807296+RoyFahel@users.noreply.github.com>
2025-12-04 17:57:41 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -62,8 +62,9 @@ function normalizePort(val) {
 const PORT = normalizePort(process.env.PORT);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+app.listen(8080, () => {
+  console.log(`✅ Server is running on port 8080`);
 });
+
 
 
